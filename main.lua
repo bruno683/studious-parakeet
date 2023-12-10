e2251b873e6d54db3f0ff42bbe7638f6c6cd8d47
Bruno Richard <bruno683@outlook.fr>
2023-12-10 12:47:00 +0100

    Modified and cleaning main file

--- main.lua
+++ main.lua
@@ -1,46 +1,39 @@
--- Cette ligne permet d'afficher des traces dans la console pendant l'éxécution
 io.stdout:setvbuf('no')
-
--- Empèche Love de filtrer les contours des images quand elles sont redimentionnées
--- Indispensable pour du pixel art
 love.graphics.setDefaultFilter("nearest", "nearest")
-
--- Cette ligne permet de déboguer pas à pas dans ZeroBraneStudio
-if arg[#arg] == "-debug" then require("mobdebug").start() end
+-- load image file
 textures = love.graphics.newImage("images/tiles.png")
 
 -- requires
 local scene = require "scene_menu"
 local scene_manager = require "scene_manager"
  
-
-
-
-
-
-
 function love.load()
+
   scene_manager.changeScene(scene)
   scene_manager.current_scene.load()
+
 end
 
 function love.update(dt)
+
   scene_manager.current_scene:update(dt)
+
 end
 
 function love.draw()
+
     scene_manager.current_scene:draw()
+
 end
 
 function love.keypressed(key)
+
   scene_manager.current_scene.keypressed(key)
   if key == "q" or key == "Q" then
       love.event.quit()
   end
-    
-  print(key)
-  
-  end
+
+end
 
 function love.mousepressed(x, y, button)
   scene_manager.current_scene.mousepressed(x, y, button)
