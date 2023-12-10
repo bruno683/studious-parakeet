0050f838cd5c95199ddec265d2be0ac2624d00fa
Bruno Richard <bruno683@outlook.fr>
2023-12-10 11:21:19 +0100

    Use alternative mouse input to draw or delete Tile

--- scene_editor.lua
+++ scene_editor.lua
@@ -9,7 +9,28 @@ function Scene.load()
 end
 
 
+function changeTile(x, y, tile)
+    local tileX = math.floor(x / map.TILESIZE) + 1
+    print("colonne", tileX)
+    local tileY = math.floor(y / map.TILESIZE) + 1
+    print("ligne", tileY)
+    if tileX >= 1 and tileX <= map.MAPSIZE and tileY >= 1 and tileY <= map.MAPSIZE  then
+        -- affichage de la tuile de notre choix par action du click
+        map.Grid[tileX][tileY] = tile
+    else
+        print("Erreur! click en dehors de la grille")
+    end
+end
+
 function Scene.update(dt)
+    local leftB = love.mouse.isDown(1)
+    local rightB = love.mouse.isDown(2) 
+    local x, y = love.mouse.getPosition()
+    if leftB then  
+        changeTile(x,y,33)
+    elseif rightB then 
+        changeTile(x, y, 0)
+    end
 end
 
 
@@ -37,16 +58,6 @@ function Scene.keypressed(key)
 end
 
 function Scene.mousepressed(x, y, button)
-    local tileX = math.floor(x / 32) + 1
-    print("colonne", tileX)
-    local tileY = math.floor(y / 32) + 1
-    print("ligne", tileY)
-    if tileX >= 1 and tileX <= map.MAPSIZE and tileY >= 1 and tileY <= map.MAPSIZE  then
-        -- affichage de la tuile de notre choix par action du click
-        map.Grid[tileX][tileY] = 33
-    else
-        print("Erreur! click en dehors de la grille")
-    end
 end
 
 
